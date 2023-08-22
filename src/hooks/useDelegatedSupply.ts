724e52c8535fda63189031ab9140782c7218ad83
cuteolaf <oliverlim818@gmail.com>
2023-08-22 08:25:07 -0400

    fix lint

--- src/hooks/useDelegatedSupply.ts
+++ src/hooks/useDelegatedSupply.ts
@@ -3,29 +3,29 @@ import { useEffect, useState } from "react";
 import { useApi } from "../contexts";
 
 export function useDelegatedSupply() {
-    const [delegated, setDelegated] = useState<Decimal>();
-    const { state: { api } } = useApi();
+	const [delegated, setDelegated] = useState<Decimal>();
+	const { state: { api } } = useApi();
 
-    useEffect(() => {
-        const fetchDelegatedSupply = async () => {
-            if (!api || !api.isReady) return;
-            const res_bytes = await api.rpc.delegateInfo.getDelegates();
-            if (res_bytes.isEmpty) {
-                console.log("Failed to get delegates");
-                return;
-            }
-            const res = api.createType("Vec<DelegateInfo>", res_bytes);
-            const data = res.toJSON();
+	useEffect(() => {
+		const fetchDelegatedSupply = async () => {
+			if (!api || !api.isReady) return;
+			const res_bytes = await api.rpc.delegateInfo.getDelegates();
+			if (res_bytes.isEmpty) {
+				console.log("Failed to get delegates");
+				return;
+			}
+			const res = api.createType("Vec<DelegateInfo>", res_bytes);
+			const data = res.toJSON();
 
-            const sum = data.reduce(
-                (sum: number, current: any) =>
-                    sum + current.nominators.reduce((total: number, item: any) => total + item[1], 0),
-                0
-            );
-            setDelegated(sum);
-        };
-        fetchDelegatedSupply();
-    }, [api]);
+			const sum = data.reduce(
+				(sum: number, current: any) =>
+					sum + current.nominators.reduce((total: number, item: any) => total + item[1], 0),
+				0
+			);
+			setDelegated(sum);
+		};
+		fetchDelegatedSupply();
+	}, [api]);
 
-    return delegated;
+	return delegated;
 }
