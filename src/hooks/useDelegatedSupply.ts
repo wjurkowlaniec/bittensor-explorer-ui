d93d84401b639aac63a21c774b3a2a5702f08cbb
Nathanael Liu <supernathanliu@gmail.com>
2023-10-11 12:57:55 -0400

    update delegated supply hook with subtensor module (#138)

--- src/hooks/useDelegatedSupply.ts
+++ src/hooks/useDelegatedSupply.ts
@@ -1,31 +1,29 @@
 import Decimal from "decimal.js";
 import { useEffect, useState } from "react";
 import { useApi } from "../contexts";
+import { rawAmountToDecimal } from "../utils/number";
 
 export function useDelegatedSupply() {
+	const {
+		state: { api, apiState },
+	} = useApi();
 	const [delegated, setDelegated] = useState<Decimal>();
-	const { state: { api } } = useApi();
+	const [unsub, setUnsub] = useState<any>(undefined);
+
+	const subscribeDelegatedSupply = async () => {
+		if (!api || apiState !== "READY") return;
+		const _unsub = await api.query.subtensorModule?.totalStake((value: any) => {
+			value && setDelegated(rawAmountToDecimal(value.toString()));
+		});
+		setUnsub(_unsub);
+	};
 
 	useEffect(() => {
-		const fetchDelegatedSupply = async () => {
-			if (!api || !api.isReady) return;
-			const res_bytes = await api.rpc.delegateInfo.getDelegates();
-			if (res_bytes.isEmpty) {
-				console.log("Failed to get delegates");
-				return;
-			}
-			const res = api.createType("Vec<DelegateInfo>", res_bytes);
-			const data = res.toJSON();
-
-			const sum = data.reduce(
-				(sum: number, current: any) =>
-					sum + current.nominators.reduce((total: number, item: any) => total + item[1], 0),
-				0
-			);
-			setDelegated(sum);
+		subscribeDelegatedSupply();
+		return () => {
+			if (unsub) unsub.then();
 		};
-		fetchDelegatedSupply();
-	}, [api]);
+	}, [apiState]);
 
 	return delegated;
 }
