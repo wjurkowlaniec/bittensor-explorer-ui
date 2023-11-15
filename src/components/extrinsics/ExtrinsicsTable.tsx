f4e6347be9f9330ad460cf356e7c959002a484b3
Nathanael Liu <supernathanliu@gmail.com>
2023-11-15 14:29:05 -0800

    Add icons for extrinsic result (#183)

--- src/components/extrinsics/ExtrinsicsTable.tsx
+++ src/components/extrinsics/ExtrinsicsTable.tsx
@@ -1,3 +1,5 @@
+/** @jsxImportSource @emotion/react */
+import { Theme, css } from "@emotion/react";
 import { NETWORK_CONFIG } from "../../config";
 import { Extrinsic } from "../../model/extrinsic";
 import { PaginatedResource } from "../../model/paginatedResource";
@@ -8,8 +10,18 @@ import { ButtonLink } from "../ButtonLink";
 import { ItemsTable, ItemsTableAttribute } from "../ItemsTable";
 import { Link } from "../Link";
 
+const successStyle = (theme: Theme) => css`
+  font-size: 16px;
+  color: ${theme.palette.success.main};
+`;
+
+const failedStyle = (theme: Theme) => css`
+  font-size: 16px;
+  color: ${theme.palette.error.main};
+`;
+
 export type ExtrinsicsTableProps = {
-	extrinsics: PaginatedResource<Extrinsic>,
+	extrinsics: PaginatedResource<Extrinsic>;
 	showAccount?: boolean;
 	showTime?: boolean;
 };
@@ -17,35 +29,31 @@ export type ExtrinsicsTableProps = {
 const ExtrinsicsTableAttribute = ItemsTableAttribute<Extrinsic>;
 
 function ExtrinsicsTable(props: ExtrinsicsTableProps) {
-	const {
-		extrinsics,
-		showAccount,
-		showTime,
-	} = props;
+	const { extrinsics, showAccount, showTime } = props;
 
 	return (
 		<ItemsTable
 			data={extrinsics.data}
 			loading={extrinsics.loading}
 			notFound={extrinsics.notFound}
-			notFoundMessage='No extrinsics found'
+			notFoundMessage="No extrinsics found"
 			error={extrinsics.error}
 			pagination={extrinsics.pagination}
-			data-test='extrinsics-table'
+			data-test="extrinsics-table"
 		>
 			<ExtrinsicsTableAttribute
-				label='ID'
+				label="ID"
 				render={(extrinsic) => (
 					<Link to={`/extrinsic/${extrinsic.id}`}>{extrinsic.id}</Link>
 				)}
 			/>
 			<ExtrinsicsTableAttribute
-				label='Name'
+				label="Name"
 				render={(extrinsic) => (
 					<ButtonLink
 						to={`/search?query=${extrinsic.module}.${extrinsic.call}`}
-						size='small'
-						color='secondary'
+						size="small"
+						color="secondary"
 					>
 						{extrinsic.module}.{extrinsic.call}
 					</ButtonLink>
@@ -53,14 +61,14 @@ function ExtrinsicsTable(props: ExtrinsicsTableProps) {
 			/>
 			{showAccount && (
 				<ExtrinsicsTableAttribute
-					label='Account'
+					label="Account"
 					render={(extrinsic) =>
 						extrinsic.signer && (
 							<AccountAddress
 								address={extrinsic.signer}
 								prefix={NETWORK_CONFIG.prefix}
 								shorten
-								copyToClipboard='small'
+								copyToClipboard="small"
 							/>
 						)
 					}
@@ -68,7 +76,7 @@ function ExtrinsicsTable(props: ExtrinsicsTableProps) {
 			)}
 			{showTime && (
 				<ExtrinsicsTableAttribute
-					label='Time'
+					label="Time"
 					render={(extrinsic) => (
 						<BlockTimestamp
 							blockHeight={extrinsic.blockHeight}
@@ -79,6 +87,17 @@ function ExtrinsicsTable(props: ExtrinsicsTableProps) {
 					)}
 				/>
 			)}
+			<ExtrinsicsTableAttribute
+				label="Result"
+				align="center"
+				render={(extrinsic) =>
+					extrinsic.success ? (
+						<span css={successStyle}>&#x1F5F9;</span>
+					) : (
+						<span css={failedStyle}>&#x1F5F5;</span>
+					)
+				}
+			/>
 		</ItemsTable>
 	);
 }
