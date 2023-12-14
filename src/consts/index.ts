ad8e7064673ea9b17f465631d52a4c6e1c73faaf
cuteolaf <oliverlim818@gmail.com>
2023-12-13 16:03:56 -0800

    weight copiers

---
+++ src/consts/index.ts
@@ -0,0 +1,7 @@
+export const weightCopiers = [
+	"5CVS9d1NcQyWKUyadLevwGxg6LgBcF9Lik6NSnbe5q59jwhE",
+	"5GcBK8PDrVifV1xAf4Qkkk6KsbsmhDdX9atvk8vyKU8xdU63",
+	"5CsvRJXuR955WojnGMdok1hbhffZyB4N5ocrv82f3p5A2zVp",
+	"5HeKSHGdsRCwVgyrHchijnZJnq4wiv6GqoDLNah8R5WMfnLB",
+	"5FFM6Nvvm78GqyMratgXXvjbqZPi7SHgSQ81nyS96jBuUWgt",
+];
