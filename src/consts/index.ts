70fdd08b0cf0d8bdb1bdb489f9b28d6a3f288df7
mog <mog@mogmachine.com>
2024-01-04 10:31:09 +0000

    remove weight copier

--- src/consts/index.ts
+++ src/consts/index.ts
@@ -2,7 +2,6 @@ export const weightCopiers = [
 	"5CVS9d1NcQyWKUyadLevwGxg6LgBcF9Lik6NSnbe5q59jwhE",
 	"5GcBK8PDrVifV1xAf4Qkkk6KsbsmhDdX9atvk8vyKU8xdU63",
 	"5CsvRJXuR955WojnGMdok1hbhffZyB4N5ocrv82f3p5A2zVp",
-	"5HeKSHGdsRCwVgyrHchijnZJnq4wiv6GqoDLNah8R5WMfnLB",
 	"5FFM6Nvvm78GqyMratgXXvjbqZPi7SHgSQ81nyS96jBuUWgt",
 	"5G1NjW9YhXLadMWajvTkfcJy6up3yH2q1YzMXDTi6ijanChe",
 	"5FKstHjZkh4v3qAMSBa1oJcHCLjxYZ8SNTSz1opTv4hR7gVB",
