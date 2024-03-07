4195f822a372df7aa80d64db93149798fdf8dad6
TopETH <supernathanliu@gmail.com>
2024-03-07 16:01:20 -0500

    feat: add subnets endpoint

--- src/services/fetchService.ts
+++ src/services/fetchService.ts
@@ -2,6 +2,7 @@ import {
 	DICTIONARY_ENDPOINT,
 	HISTORICAL_ENDPOINT,
 	INDEXER_ENDPOINT,
+	SUBNETS_ENDPOINT,
 } from "../config";
 import { fetchGraphql } from "../utils/fetchGraphql";
 
@@ -24,4 +25,11 @@ export async function fetchHistorical<T = any>(
 	variables: object = {}
 ) {
 	return fetchGraphql<T>(HISTORICAL_ENDPOINT, query, variables);
+}
+
+export async function fetchSubnets<T = any>(
+	query: string,
+	variables: object = {}
+) {
+	return fetchGraphql<T>(SUBNETS_ENDPOINT, query, variables);
 }
