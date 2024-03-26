6fc9c03fb1760700f2d1ea76de8695a9ca3bd41d
mog <mog@mogmachine.com>
2024-03-26 14:47:09 +0000

    Update index.ts

--- src/consts/index.ts
+++ src/consts/index.ts
@@ -4,7 +4,6 @@ export const weightCopiers = [
 	"5CsvRJXuR955WojnGMdok1hbhffZyB4N5ocrv82f3p5A2zVp",
 	"5FFM6Nvvm78GqyMratgXXvjbqZPi7SHgSQ81nyS96jBuUWgt",
 	"5G1NjW9YhXLadMWajvTkfcJy6up3yH2q1YzMXDTi6ijanChe",
-	"5FKstHjZkh4v3qAMSBa1oJcHCLjxYZ8SNTSz1opTv4hR7gVB",
 	"5HeKSHGdsRCwVgyrHchijnZJnq4wiv6GqoDLNah8R5WMfnLB",
 	"5HbScNssaEfioJHXjcXdpyqo1AKnYjymidGF8opcF9rTFZdT",
 ];
